d65a842835cf1c3ec86d4518a44b8f9717530b32
tor-nav <tor.roneid@nav.no>
2021-11-09 16:46:45 +0100

    lag input for uuid

--- packages/behandling-tilbakekreving/src/IndexOrInput.tsx
+++ packages/behandling-tilbakekreving/src/IndexOrInput.tsx
@@ -1,52 +1,80 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import { alleKodeverk } from '@fpsak-frontend/storybook-utils';
 import BehandlingTilbakekrevingIndex from './BehandlingTilbakekrevingIndex';
 import BehandlingEventHandler from './BehandlingEventHandler';
 
-const IndexOrInput: FunctionComponent = () => (
-  <BehandlingTilbakekrevingIndex
-    behandlingUuid="d4674d96-d99c-40e1-b32a-ff132cc4d432"
-    fagsak={{
-      dekningsgrad: 80,
-      fagsakYtelseType: {
-        kode: 'FP',
-        kodeverk: 'FAGSAK_YTELSE',
-      },
-      relasjonsRolleType: {
-        kode: 'MORA',
-        kodeverk: 'RELASJONSROLLE_TYPE',
-      },
-      saksnummer: '152001002',
-      status: {
-        kode: 'LOP',
-        kodeverk: 'FAGSAK_STATUS',
-      },
-    }}
-    rettigheter={{
-      kanOverstyreAccess: {
-        employeeHasAccess: true,
-        isEnabled: true,
-      },
-      writeAccess: {
-        employeeHasAccess: true,
-        isEnabled: true,
-      },
-    }}
-    oppdaterProsessStegOgFaktaPanelIUrl={() => undefined}
-    valgtProsessSteg="default"
-    valgtFaktaSteg="default"
-    oppdaterBehandlingVersjon={() => undefined}
-    behandlingEventHandler={BehandlingEventHandler}
-    opneSokeside={() => undefined}
-    setRequestPendingMessage={() => undefined}
-    // @ts-ignore
-    kodeverk={alleKodeverk}
-    fagsakKjønn={{
-      kode: 'K',
-      kodeverk: 'KJONN',
-    }}
-    harApenRevurdering={false}
-  />
-);
+const IndexOrInput: FunctionComponent = () => {
+  const [behandlingUuid, setBehandlingUuid] = useState<string | undefined>();
+  const [valgtProsessSteg, setProsessSteg] = useState('default');
+  const [valgtFaktaSteg, setFaktaSteg] = useState('default');
+
+  if (!behandlingUuid) {
+    return (
+      <>
+        BehandlingUuid:
+        <input type="text" id="bUuid" />
+        <button
+          type="button"
+          onClick={() => {
+            setBehandlingUuid((document.getElementById('bUuid') as HTMLInputElement).value);
+          }}
+        >
+          Hent behandling
+        </button>
+      </>
+    );
+  }
+
+  const oppdaterProsessStegOgFaktaPanelIUrl = (punktnavn?: string, faktanavn?: string) => {
+    setProsessSteg(punktnavn);
+    setFaktaSteg(faktanavn);
+  };
+
+  return (
+    <BehandlingTilbakekrevingIndex
+      behandlingUuid={behandlingUuid}
+      fagsak={{
+        dekningsgrad: 100,
+        fagsakYtelseType: {
+          kode: 'FP',
+          kodeverk: 'FAGSAK_YTELSE',
+        },
+        relasjonsRolleType: {
+          kode: 'MORA',
+          kodeverk: 'RELASJONSROLLE_TYPE',
+        },
+        saksnummer: '152001002',
+        status: {
+          kode: 'LOP',
+          kodeverk: 'FAGSAK_STATUS',
+        },
+      }}
+      rettigheter={{
+        kanOverstyreAccess: {
+          employeeHasAccess: true,
+          isEnabled: true,
+        },
+        writeAccess: {
+          employeeHasAccess: true,
+          isEnabled: true,
+        },
+      }}
+      oppdaterProsessStegOgFaktaPanelIUrl={oppdaterProsessStegOgFaktaPanelIUrl}
+      valgtProsessSteg={valgtProsessSteg}
+      valgtFaktaSteg={valgtFaktaSteg}
+      oppdaterBehandlingVersjon={() => undefined}
+      behandlingEventHandler={BehandlingEventHandler}
+      opneSokeside={() => undefined}
+      setRequestPendingMessage={() => undefined}
+      // @ts-ignore
+      kodeverk={alleKodeverk}
+      fagsakKjønn={{
+        kode: 'K',
+        kodeverk: 'KJONN',
+      }}
+      harApenRevurdering={false}
+    />
+  );
+};
 
 export default IndexOrInput;
